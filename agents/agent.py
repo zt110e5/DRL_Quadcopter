0ff148d1ad20383bd833f826444f188e30b9d317
ztgit <18706757080@163.com>
2018-05-25 10:07:10 +0800

    First
    first add Quadcopter

---
+++ agents/agent.py
@@ -0,0 +1,295 @@
+from keras import layers, models, optimizers
+from keras import backend as K
+import numpy as np
+import random
+
+class Actor:
+    """Actor (Policy) Model."""
+
+    def __init__(self, state_size, action_size, action_low, action_high):
+        """Initialize parameters and build model.
+        Params
+        ======
+            state_size (int): Dimension of each state
+            action_size (int): Dimension of each action
+            action_low (array): Min value of each action dimension
+            action_high (array): Max value of each action dimension
+        """
+        self.state_size = state_size
+        self.action_size = action_size
+        self.action_low = action_low
+        self.action_high = action_high
+        self.action_range = self.action_high - self.action_low
+        self.build_model()
+
+    def build_model(self):
+        """Build an actor (policy) network that maps states -> actions."""
+
+        # 输入层(states)
+        states = layers.Input(shape=(self.state_size,), name='states')
+
+        # 隐藏层
+        net = layers.Dense(units=32, activation='relu')(states)
+        net = layers.BatchNormalization()(net)
+        net = layers.Dropout(0.5)(net)
+        net = layers.Dense(units=64, activation='relu')(net)
+        net = layers.BatchNormalization()(net)
+        net = layers.Dropout(0.5)(net)
+        net = layers.Dense(units=32, activation='relu')(net)
+        net = layers.BatchNormalization()(net)
+        net = layers.Dropout(0.5)(net)
+
+        # 输出层
+        raw_actions = layers.Dense(units=self.action_size, activation='sigmoid',
+                                   name='raw_actions')(net)
+
+        # 将每个动作的[0,1]输出缩放到适当的维度范围
+        actions = layers.Lambda(lambda x: (x * self.action_range) + self.action_low,
+                                name='actions')(raw_actions)
+
+        
+        self.model = models.Model(inputs=states, outputs=actions)
+
+        # 损失函数
+        action_gradients = layers.Input(shape=(self.action_size,))
+        loss = K.mean(-action_gradients * actions)
+
+        # 优化策略
+        optimizer = optimizers.Adam()
+        updates_op = optimizer.get_updates(params=self.model.trainable_weights, loss=loss)
+        self.train_fn = K.function(
+            inputs=[self.model.input, action_gradients, K.learning_phase()],
+            outputs=[],
+            updates=updates_op)
+
+class Critic:
+    """Critic (Value) Model."""
+
+    def __init__(self, state_size, action_size):
+        """Initialize parameters and build model.
+        Params
+        ======
+            state_size (int): Dimension of each state
+            action_size (int): Dimension of each action
+        """
+        self.state_size = state_size
+        self.action_size = action_size
+        self.build_model()
+
+    def build_model(self):
+        """Build a critic (value) network that maps (state, action) pairs -> Q-values."""
+
+        # 输入层
+        states = layers.Input(shape=(self.state_size,), name='states')
+        actions = layers.Input(shape=(self.action_size,), name='actions')
+
+        # 隐藏层-->state 
+        net_states = layers.Dense(units=32, activation='relu')(states)
+        net_states = layers.BatchNormalization()(net_states)
+        net_states = layers.Dropout(0.5)(net_states)
+        net_states = layers.Dense(units=64, activation='relu')(net_states)
+        net_states = layers.BatchNormalization()(net_states)
+        net_states = layers.Dropout(0.5)(net_states)
+        net_states = layers.Dense(units=32, activation='relu')(net_states)
+        net_states = layers.BatchNormalization()(net_states)
+        net_states = layers.Dropout(0.5)(net_states)
+
+        # 隐藏层-->action
+        net_actions = layers.Dense(units=32, activation='relu')(actions)
+        net_actions = layers.BatchNormalization()(net_actions)
+        net_actions = layers.Dropout(0.5)(net_actions)
+        net_actions = layers.Dense(units=64, activation='relu')(net_actions)
+        net_actions = layers.BatchNormalization()(net_actions)
+        net_actions = layers.Dropout(0.5)(net_actions)
+        net_actions = layers.Dense(units=32, activation='relu')(net_actions)
+        net_actions = layers.BatchNormalization()(net_actions)
+        net_actions = layers.Dropout(0.5)(net_actions)
+
+        # 整合state and action
+        net = layers.Add()([net_states, net_actions])
+        net = layers.Activation('relu')(net)
+
+        # 输出层
+        Q_values = layers.Dense(units=1, name='q_values')(net)
+
+        
+        self.model = models.Model(inputs=[states, actions], outputs=Q_values)
+
+        # 优化策略
+        optimizer = optimizers.Adam()
+        self.model.compile(optimizer=optimizer, loss='mse')
+
+        # Compute action gradients (derivative of Q values w.r.t. to actions)
+        action_gradients = K.gradients(Q_values, actions)
+
+        # Define an additional function to fetch action gradients (to be used by actor model)
+        self.get_action_gradients = K.function(
+            inputs=[*self.model.input, K.learning_phase()],
+            outputs=action_gradients)
+
+class OUNoise:
+    """Ornstein-Uhlenbeck process."""
+
+    def __init__(self, size, mu, theta, sigma):
+        """Initialize parameters and noise process."""
+        self.mu = mu * np.ones(size)
+        self.theta = theta
+        self.sigma = sigma
+        self.reset()
+
+    def reset(self):
+        """Reset the internal state (= noise) to mean (mu)."""
+        self.state = self.mu
+
+    def sample(self):
+        """Update internal state and return it as a noise sample."""
+        x = self.state
+        dx = self.theta * (self.mu - x) + self.sigma * np.random.randn(len(x))
+        self.state = x + dx
+        return self.state
+
+from collections import namedtuple, deque
+
+class ReplayBuffer:
+    """Fixed-size buffer to store experience tuples."""
+
+    def __init__(self, buffer_size, batch_size):
+        """Initialize a ReplayBuffer object.
+        Params
+        ======
+            buffer_size: maximum size of buffer
+            batch_size: size of each training batch
+        """
+        self.memory = deque(maxlen=buffer_size)  # internal memory (deque)
+        self.batch_size = batch_size
+        self.experience = namedtuple("Experience", field_names=["state", "action", "reward", "next_state", "done"])
+
+    def add(self, state, action, reward, next_state, done):
+        """Add a new experience to memory."""
+        e = self.experience(state, action, reward, next_state, done)
+        self.memory.append(e)
+
+    def sample(self, batch_size=64):
+        """Randomly sample a batch of experiences from memory."""
+        return random.sample(self.memory, k=self.batch_size)
+
+    def __len__(self):
+        """Return the current size of internal memory."""
+        return len(self.memory)
+
+class DDPG():
+
+    def __init__(self, task):
+        self.task = task
+        self.state_size = task.state_size
+        self.action_size = task.action_size
+        self.action_low = task.action_low
+        self.action_high = task.action_high
+
+        # Actor (Policy) Model
+        self.actor_local = Actor(self.state_size, self.action_size, self.action_low, self.action_high)
+        self.actor_target = Actor(self.state_size, self.action_size, self.action_low, self.action_high)
+
+        # Critic (Value) Model
+        self.critic_local = Critic(self.state_size, self.action_size)
+        self.critic_target = Critic(self.state_size, self.action_size)
+
+        # Initialize target model parameters with local model parameters
+        self.critic_target.model.set_weights(self.critic_local.model.get_weights())
+        self.actor_target.model.set_weights(self.actor_local.model.get_weights())
+
+        # Noise process
+        self.exploration_mu = 0
+        self.exploration_theta = 0.08
+        self.exploration_sigma = 0.15
+        self.noise = OUNoise(self.action_size, self.exploration_mu, self.exploration_theta, self.exploration_sigma)
+
+        # Replay memory
+        self.buffer_size = 100000
+        self.batch_size = 64
+        self.memory = ReplayBuffer(self.buffer_size, self.batch_size)
+
+        # Algorithm parameters
+        self.gamma = 0.95  # a discount factor
+        self.tau = 0.001  # soft update of target parameters
+
+        # Score tracker and learning parameters
+        self.score = 0
+        self.best_score = -np.inf
+        self.count = 0
+
+    def reset_episode(self):
+        self.total_reward = 0.0
+        self.count = 0
+        self.noise.reset()
+        state = self.task.reset()
+        self.last_state = state
+        return state
+
+    def step(self, action, reward, next_state, done):
+
+        # Save (experience) reward
+        self.total_reward += reward
+        self.count += 1
+
+        # Save (experience) reward
+        self.memory.add(self.last_state, action, reward, next_state, done)
+
+        # Learn, if enough samples are available in memory
+        if len(self.memory) > self.batch_size:
+            experiences = self.memory.sample()
+            self.learn(experiences)
+
+        # Roll over last state and action
+        self.last_state = next_state
+
+    def act(self, states):
+        # Action based on given state and policy
+        states = np.reshape(states, [-1, self.state_size])
+        action = self.actor_local.model.predict(states)[0]
+        return list(action + self.noise.sample())
+
+    def learn(self, experiences):
+        """Update policy and value parameters using given batch of reward tuples."""
+
+        # Turn reward experience into separate arrays for each element (states, actions, rewards, etc.)
+        states = np.vstack([e.state for e in experiences if e is not None])
+        actions = np.array([e.action for e in experiences if e is not None]).astype(np.float32).reshape(-1,
+                                                                                                        self.action_size)
+        rewards = np.array([e.reward for e in experiences if e is not None]).astype(np.float32).reshape(-1, 1)
+        dones = np.array([e.done for e in experiences if e is not None]).astype(np.uint8).reshape(-1, 1)
+        next_states = np.vstack([e.next_state for e in experiences if e is not None])
+
+        # Get predicted actions of next-state  and Q values from target models
+        actions_next = self.actor_target.model.predict_on_batch(next_states)
+        Q_targets_next = self.critic_target.model.predict_on_batch([next_states, actions_next])
+
+        # Compute Q targets for current states and train critic model (local)
+        Q_targets = rewards + self.gamma * Q_targets_next * (1 - dones)
+        self.critic_local.model.train_on_batch(x=[states, actions], y=Q_targets)
+
+        # Train actor model (local)
+        action_gradients = np.reshape(self.critic_local.get_action_gradients([states, actions, 0]),
+                                      (-1, self.action_size))
+        # Custom training function
+        self.actor_local.train_fn([states, action_gradients, 1])
+
+        # Soft-update target models
+        self.soft_update(self.critic_local.model, self.critic_target.model)
+        self.soft_update(self.actor_local.model, self.actor_target.model)
+
+        # Get the best score
+        self.score = self.total_reward / float(self.count) if self.count else 0.0
+        if self.score > self.best_score:
+            self.best_score = self.score
+
+    def soft_update(self, local_model, target_model):
+
+        """Soft update model parameters."""
+        local_weights = np.array(local_model.get_weights())
+        target_weights = np.array(target_model.get_weights())
+
+        assert len(local_weights) == len(target_weights), "Local and target model parameters must have the same size"
+
+        new_weights = self.tau * local_weights + (1 - self.tau) * target_weights
+        target_model.set_weights(new_weights)
